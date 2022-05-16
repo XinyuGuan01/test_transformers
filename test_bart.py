9dd85ed2783f330cd64c597e738362bc249a1f9d
Guan Xinyu <48087162+XinyuGuan01@users.noreply.github.com>
2022-05-16 20:33:01 +0800

    debug

--- test_bart.py
+++ test_bart.py
@@ -1,7 +1,7 @@
 from transformers import BartForConditionalGeneration, AutoTokenizer, Text2TextGenerationPipeline
 import torch
 
-tokenizer=AutoTokenizer.from_pretrained('IDEA-CCNL/Randeng-BART-139M', use_fast=false)
+tokenizer=AutoTokenizer.from_pretrained('IDEA-CCNL/Randeng-BART-139M', use_fast=False)
 model=BartForConditionalGeneration.from_pretrained('IDEA-CCNL/Randeng-BART-139M')
 text = '桂林市是世界闻名<mask> ，它有悠久的<mask>'
 text2text_generator = Text2TextGenerationPipeline(model, tokenizer)
